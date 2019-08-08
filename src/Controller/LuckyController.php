287a6e6eaeb0a4b108ff68d77dc7706765db6508
miaguirre22 <aguirremartinignacio@gmail.com>
2019-08-08 12:51:29 -0300

    A Simple Controller

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -5,21 +5,25 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Routing\Tests\Fixtures\AnnotationFixtures\AbstractClassController;
+
+//use Symfony\Component\Routing\Tests\Fixtures\AnnotationFixtures\AbstractClassController;
 
 class LuckyController extends AbstractController
 {
     /**
-     * @Route("/lucky/number")
+     * @Route("/lucky/number/{max}", name="app_lucky_number")
      */
-    public function number()
+    public function number($max)
     {
-        $number = random_int(0, 100);
+        $number = random_int(0, $max);
 
-        //return new Response('<html><body>Lucky number: '.$number.'</body></html>'
+        //return new Response('<html><body>Lucky number: '.$number.'</body></html>');
+
         return $this->render('lucky/number.html.twig', [
             'number' => $number,
         ]
         );
     }
+
+
 }
