843e7e23e08da2f1458a22dc8bf430483db69250
miaguirre22 <aguirremartinignacio@gmail.com>
2019-08-09 10:57:34 -0300

    Creating an Entity Class

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Psr\Log\LoggerInterface;
 
 //use Symfony\Component\Routing\Tests\Fixtures\AnnotationFixtures\AbstractClassController;
 
@@ -14,9 +15,11 @@ class LuckyController extends AbstractController
     /**
      * @Route("/lucky/number/{max?}", name="app_lucky_number", requirements={"max"="\d+"})
      */
-    public function number($max)
+    public function number($max, LoggerInterface $logger)
     {
         //$url = $this->generateUrl('app_lucky_number', ['max' => 45]);
+
+        $logger->info('Estamos logueados!');
 
         $number = random_int(0, $max);
 
@@ -24,30 +27,9 @@ class LuckyController extends AbstractController
 
         return $this->render('lucky/number.html.twig', [
             'number' => $number,
+            //'logger' => $logger
         ]
         );
-    }
-
-// ...
-    public function index()
-    {
-        // redirects to the "homepage" route
-        return $this->redirectToRoute('homepage');
-
-        // redirectToRoute is a shortcut for:
-        // return new RedirectResponse($this->generateUrl('homepage'));
-
-        // does a permanent - 301 redirect
-        return $this->redirectToRoute('homepage', [], 301);
-
-        // redirect to a route with parameters
-        return $this->redirectToRoute('app_lucky_number', ['max' => 10]);
-
-        // redirects to a route and maintains the original query string parameters
-        return $this->redirectToRoute('blog_show', $request->query->all());
-
-        // redirects externally
-        return $this->redirect('http://symfony.com/doc');
     }
 
 }
